eb8f52caf0857c570a99503052a77bc3dea7378e
NEsfandiari <nesfandiari37@gmail.com>
2018-07-25 20:13:37 -0700

    build scripts

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
+  proxy: {
+    prefix: '/localhost:9000',
+    url: 'http:/',
+  },
   plugins: [
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-styled-components`,
     'gatsby-plugin-stripe-checkout',
+    `gatsby-plugin-netlify`,
   ],
 }
