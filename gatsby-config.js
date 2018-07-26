694644f54253f689dbdfc0f8c75cbe6b37998c95
NEsfandiari <nesfandiari37@gmail.com>
2018-07-25 20:06:18 -0700

    build scripts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack')
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
@@ -7,11 +6,5 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-styled-components`,
     'gatsby-plugin-stripe-checkout',
-    new webpack.DefinePlugin({
-      LAMBDA_ENDPOINT: JSON.stringify(process.env.LAMBDA_ENDPOINT),
-      STRIPE_PUBLISHABLE_KEY: JSON.stringify(
-        process.env.STRIPE_PUBLISHABLE_KEY
-      ),
-    }),
   ],
 }
