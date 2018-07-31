8a3b20e1a539f282ffad32841efbcd1f58889347
NEsfandiari <nesfandiari37@gmail.com>
2018-07-30 21:52:21 -0700

    contenful integration complete

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,14 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-stripe-checkout`,
     `gatsby-plugin-stripe-elements`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+        host: `cdn.contentful.com`,
+      },
+    },
     `gatsby-plugin-netlify`,
   ],
 }
