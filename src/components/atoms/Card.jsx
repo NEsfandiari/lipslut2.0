f8ab22d1339f8c1257655e4a0df8bb9c04a7c43a
NEsfandiari <nesfandiari37@gmail.com>
2018-09-28 17:52:00 -0700

    style tweaks

--- src/components/atoms/Card.jsx
+++ src/components/atoms/Card.jsx
@@ -5,7 +5,7 @@ const Card = styled.div`
   flex-direction: column;
   align-items: ${({ alignItems }) => alignItems || 'center'};
   padding: 2.5rem;
-  box-shadow: 5px 5px 25px 0 rgba(46, 61, 73, 0.2);
+  box-shadow: 3px 3px 15px 0 rgba(46, 61, 73, 0.2);
   background: white;
   border-radius: 6px;
   width: 400px;
