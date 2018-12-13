2f302b7c79244c2776b1d1f19f14763772ef1b4c
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2018-12-13 11:35:39 -0800

    Added some code to test updateAccount lambda src.

--- lambda-src/updateAccount.js
+++ lambda-src/updateAccount.js
@@ -10,6 +10,9 @@ const shopifyConfig = {
   'X-Shopify-Storefront-Access-Token':
     process.env.GATSBY_SHOPIFY_STOREFRONT_KEY,
 }
+
+let updateAccountResponse
+
 exports.handler = async function(event, context, callback) {
   // TEST for POST request
   if (event.httpMethod !== 'POST' || !event.body) {
@@ -23,6 +26,63 @@ exports.handler = async function(event, context, callback) {
     let data = JSON.parse(event.body)
     data = JSON.parse(data.body)
 
-    // INSERT CODE HERE
+    // get customerAccessToken
+    const payload1 = {
+      query: `mutation customerAccessTokenCreate($input: CustomerAccessTokenCreateInput!) {
+        customerAccessTokenCreate(input: $input) {
+          userErrors {
+            field
+            message
+          }
+          customerAccessToken {
+            accessToken
+            expiresAt
+          }
+          customerUserErrors {
+            field
+            message
+          }
+        }
+      }
+      `,
+      variables: {
+        input: {
+          email: data.email,
+          password: data.uid,
+        },
+      },
+    }
+    let token
+    try {
+      token = await axios({
+        url: 'https://lipslut2-0.myshopify.com/api/graphql',
+        method: 'POST',
+        headers: shopifyConfig,
+        data: JSON.stringify(payload1),
+      })
+
+      updateAccountResponse = token.data.data
+
+      if (token.data.data.customerAccessTokenCreate.userErrors.length > 0)
+        throw token.data.data.customerAccessTokenCreate.userErrors
+      else
+        token =
+          token.data.data.customerAccessTokenCreate.customerAccessToken
+            .accessToken
+    } catch (err) {
+      console.log(err[0])
+      let response = {
+        statusCode: 500,
+        headers,
+        body: JSON.stringify({
+          error: err[0],
+        }),
+      }
+      callback(null, response)
+    }
+
+    console.log('updateAccountResponse is: ', updateAccountResponse)
+
+    // update customer data
   }
 }
