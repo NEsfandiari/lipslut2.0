f61d2bdb5fce614a757b05a4159bee818726c24c
Sarah Kaplan <skaplan520@gmail.com>
2018-12-13 16:08:17 -0800

    added accessibility features to DropDownMenu component for navbar

--- src/components/atoms/DropdownMenu.jsx
+++ src/components/atoms/DropdownMenu.jsx
@@ -40,9 +40,12 @@ const Container = styled.div`
 class DropdownMenu extends Component {
   constructor(props) {
     super(props)
-    this.dropdown = React.createRef()
+    this.myRefs = this.props.links.map(link => React.createRef())
     this.focus = this.focus.bind(this)
+
+    console.log('REFS CONSTRUCTOR, ', this.myRefs)
   }
+
   state = {
     display: false,
   }
@@ -52,52 +55,137 @@ class DropdownMenu extends Component {
   hideMenu = () => {
     this.setState({ display: false })
   }
-  focus() {
-    console.log('focus ran! ', this.dropdown.current)
-    this.innerRef.focus()
+  focus(focusTarget) {
+    console.log('focus ran! ', focusTarget)
+    focusTarget.focus()
   }
 
-  toggleMenu() {
+  toggleMenu(focusTarget) {
     console.log('toggling!')
-    this.setState(st => ({
-      display: !st.display,
-    }))
+    this.setState(
+      st => ({
+        display: !st.display,
+      }),
+      () => this.focus(focusTarget)
+      // () => {
+      //   if (this.state.display === true) {
+      //     this.focus(focusTarget)
+      //   }
+      // }
+    )
   }
 
   render() {
-    console.log('here are mahh refs: ', this.dropdown)
     const display = this.state.display ? 'initial' : 'none'
-    const links = this.props.links.map((link, i) => (
-      <NavLink
-        to={link.route}
-        onClick={link.name === 'Log Out' ? this.props.logOut : null}
-        key={i}
-        innerRef={el => (this.innerRef = el)}
-      >
-        {link.name}
-      </NavLink>
-    ))
+    // const links = this.props.links.map((link, i, array) => {
+    //   if (i === 0) {
+    //     return (
+    //       <NavLink
+    //         to={link.route}
+    //         onClick={link.name === 'Log Out' ? this.props.logOut : null}
+    //         key={i}
+    //         innerRef={el => (this.dropdownTop = el)}
+    //         onKeyDown={e => {
+    //           console.log('trying these: ', e.key)
+    //           if (e.key === 'Enter' || e.key === ' ') {
+    //             this.hideMenu()
+    //           } else if (e.key === 'ArrowUp') {
+    //             this.focus(this.dropdownBottom)
+    //           } else if (e.key === 'ArrowDown') {
+    //             this.focus()
+    //           }
+    //         }}
+    //       >
+    //         {link.name}
+    //       </NavLink>
+    //     )
+    //   } else if (i === array.length - 1) {
+    //     return (
+    //       <NavLink
+    //         to={link.route}
+    //         onClick={link.name === 'Log Out' ? this.props.logOut : null}
+    //         key={i}
+    //         innerRef={el => (this.dropdownBottom = el)}
+    //       >
+    //         {link.name}
+    //       </NavLink>
+    //     )
+    //   } else {
+    //     return (
+    //       <NavLink
+    //         to={link.route}
+    //         onClick={link.name === 'Log Out' ? this.props.logOut : null}
+    //         key={i}
+    //         innerRef={el => (this.innerRef = el)}
+    //       >
+    //         {link.name}
+    //       </NavLink>
+    //     )
+    //   }
+    // })
+
+    const links = this.props.links.map((link, i, array) => {
+      return (
+        <NavLink
+          to={link.route}
+          onClick={link.name === 'Log Out' ? this.props.logOut : null}
+          key={i}
+          innerRef={el => (this.myRefs[i] = el)}
+          onKeyDown={e => {
+            console.log('KEY ', e.key)
+            if (e.key === 'ArrowDown' && i !== array.length - 1) {
+              this.focus(this.myRefs[i + 1])
+            } else if (e.key === 'ArrowUp' && i !== 0) {
+              this.focus(this.myRefs[i - 1])
+            } else if (e.key === 'ArrowDown') {
+              this.focus(this.myRefs[0])
+            } else if (e.key === 'ArrowUp') {
+              this.focus(this.myRefs[array.length - 1])
+            } else if (e.key === 'Escape' || e.key === 'Enter') {
+              this.hideMenu()
+            }
+          }}
+        >
+          {link.name}
+        </NavLink>
+      )
+    })
+
     return (
-      <Container
-        onMouseLeave={this.hideMenu}
-        role="button"
-        onKeyDown={e => {
-          console.log('trying these: ', e.key)
-          if (e.key === 'Enter' || e.key === ' ') {
-            this.toggleMenu()
-            this.focus()
-          }
-        }}
-        role="button"
-        onBlur={() => {
-          console.log('i am working')
-          this.hideMenu()
-        }}
-      >
-        <NavLink className="dropdown" onMouseEnter={this.showMenu} to="">
+      <Container onMouseLeave={this.hideMenu}>
+        <NavLink
+          className="dropdown"
+          role="button"
+          aria-haspopup="true"
+          aria-expanded={this.state.display}
+          onKeyDown={e => {
+            console.log('trying these: ', e.key)
+            if (this.state.display === false) {
+              if (e.key === 'ArrowDown' || e.key === 'Enter' || e.key === ' ') {
+                this.toggleMenu(this.myRefs[0])
+              } else if (e.key === 'ArrowUp') {
+                this.toggleMenu(this.myRefs[this.myRefs.length - 1])
+              }
+            }
+          }}
+          onMouseEnter={this.showMenu}
+          to=""
+        >
           {this.props.dropdownText}
         </NavLink>
-        <div className="dropdown-content" style={{ display }}>
+        <div
+          className="dropdown-content"
+          onKeyDown={e => {
+            console.log('trying these: ', e.key)
+            if (this.state.display === true) {
+              if (e.key === ' ') {
+                this.hideMenu()
+              } else if (e.key === 'ArrowDown') {
+              }
+            }
+          }}
+          style={{ display }}
+        >
           <div className="links">{links}</div>
         </div>
       </Container>
